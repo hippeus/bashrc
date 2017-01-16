3b6218f70ed2c05a246388ea5ef59e2fa3405fd8
your name <yor eMail adress>
2017-01-16 16:11:30 +0100

    Minor: Addition support for colors

--- bashrc-prepare-env.sh
+++ bashrc-prepare-env.sh
@@ -6,15 +6,17 @@
 # Distributed under terms of the MIT license.
 #
 
+source colors.inc
+
 BASH_DIR="$HOME/.bash"
 BASH_PREPARE_SCRIPT_DIR=`pwd`
 
 create_directory(){
   local path_to_directory=$1;
   if [ -d ${path_to_directory} ]; then
-    echo "${path_to_directory} directory exists";
+    echo -e "[${YELLOW} Warning ${RESTORE}] Directory: ${path_to_directory} exists $ ";
   else
-    echo "creating directory: ${path_to_directory}";
+    echo -e "[${CYAN}INFO${RESTORE}] Creating directory: ${path_to_directory} ${RESTORE}";
     mkdir -p ${path_to_directory};
   fi
 }
@@ -24,7 +26,7 @@ copy_directory_recursively(){
 }
 
 download_git_aware_prompt(){
-  echo "Trying to obtain git-aware-prompt from source..."
+  echo -e "[${CYAN}INFO${RESTORE}] Trying to obtain git-aware-prompt from source..."
   ( git clone git://github.com/jimeh/git-aware-prompt.git > /dev/null 2>&1 )
 }
 
@@ -35,8 +37,8 @@ install_git_aware_prompt(){
   local returnCode=$?
   local success=0
   if [ ${returnCode} -ne ${success} ]; then
-    echo "Trying to obtain git-aware-prompt from source code failed"
-    echo "Obtaining git-aware-prompt from a current directory"
+    echo -e "[${RED}ERROR${RESTORE}] Obtain git-aware-prompt from source code failed"
+    echo -e "[${CYAN}INFO${RESTORE}] Obtaining git-aware-prompt from the current directory"
     copy_directory_recursively ${BASH_PREPARE_SCRIPT_DIR}/git-aware-prompt .
   fi
   popd
@@ -50,26 +52,28 @@ set_up_bashrc(){
   test_if_files_are_identical $HOME/.bashrc ${BASH_PREPARE_SCRIPT_DIR}/bashrc
   local success=$?
   if [ ${success} -eq 0 ]; then
-    echo "Your current $HOME/.bashrc is identical to ${BASH_PREPARE_SCRIPT_DIR}/bashrc !"
-    echo "....skipping routine"
+    echo -e "[${YELLOW}WARNINIG${RESTORE}] Your current $HOME/.bashrc is identical to ${BASH_PREPARE_SCRIPT_DIR}/bashrc !"
+    echo -e "....skipping routine"
     exit 0
   else
-    echo "Transfer of an old ~/.bashrc to ${BASH_PREPARE_SCRIPT_DIR}/bashrc.old"
+    echo -e "[${CYAN}INFO${RESTORE}] Transfer of an old ~/.bashrc to ${BASH_PREPARE_SCRIPT_DIR}/bashrc.old"
     mv $HOME/.bashrc ${BASH_PREPARE_SCRIPT_DIR}/bashrc.old
     ln -sf ${BASH_PREPARE_SCRIPT_DIR}/bashrc $HOME/.bashrc
   fi
 }
 
 setup_global_git_ignore_file(){
+  echo -e "[${CYAN}INFO${RESTORE}] ${BLUE} Setting up global git ignore config ... ${RESTORE}"
   local ignore_file=".gitignore_global"
   touch ${HOME}/${ignore_file}
-  echo "*~" >> ${HOME}/${ignore_file}
-  echo "*.swp" >> ${HOME}/${ignore_file}
-  echo ".ycm_extra_conf.py*" >> ${HOME}/${ignore_file}
+  echo -e "*~" >> ${HOME}/${ignore_file}
+  echo -e "*.swp" >> ${HOME}/${ignore_file}
+  echo -e ".ycm_extra_conf.py*" >> ${HOME}/${ignore_file}
   git config --global core.excludesfile ${HOME}/${ignore_file}
 }
 
 enhance_git_global_conf(){
+  echo -e "[${CYAN}INFO${RESTORE}] ${BLUE} Setting up global git config ... ${RESTORE}"
   setup_global_git_ignore_file
 
   git config --global diff.tool vimdiff
